b5ab5dfbc317b1551cc40bcfeed697018b286ad0
junghojang-insunetfc <jhjang@insunetfc.com>
2021-08-22 20:13:45 +0900

    [#3] vue members store 만들기

---
+++ src/store/moduleMembers.js
@@ -0,0 +1,20 @@
+export const moduleMembers = {
+  state: {
+    members: [],
+    member: {
+      name: '',
+      age: ''
+    }
+  },
+  mutations: {
+  },
+  actions: {
+    membersCreate(thisStore) {
+      thisStore.state.members.push({
+        name: thisStore.state.member.name,
+        age: thisStore.state.member.age
+      })
+      console.log('Done membersCreate', moduleMembers.state.members)
+    }
+  }
+}
